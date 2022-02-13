ee110c4881bff29ecefa94ecdf66581a535d27b2
Nikhil Menon <psynm9@exmail.nottingham.ac.uk>
2022-02-13 10:03:04 +0000

    Fixed a bug in transcribe.html

--- frontend/FlaskApplication.py
+++ frontend/FlaskApplication.py
@@ -4,8 +4,6 @@ from flask import Flask, render_template, url_for, redirect, request
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-UPLOAD_FOLDER = 'C:\\Users\\nikhi\\Desktop\\HackNotts\\videosum\\frontend\\File'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 @app.route("/")
@@ -17,8 +15,6 @@ def home():
 def upload_file():
     if request.method == 'POST':
         file = request.files['FileName']
-        filename = secure_filename(file.filename)
-        #file.save(app.config['UPLOAD_FOLDER'], filename)
         return render_template('transcribe.html')
 
 
